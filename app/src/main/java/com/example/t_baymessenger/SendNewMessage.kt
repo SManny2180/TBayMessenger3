5671a03b3918a9bbe66ac056ca8b309a9538989f
SManny2180 <80494753+SManny2180@users.noreply.github.com>
2021-04-23 19:37:36 -0400

    bugfixes

--- app/src/main/java/com/example/t_baymessenger/SendNewMessage.kt
+++ app/src/main/java/com/example/t_baymessenger/SendNewMessage.kt
@@ -2,10 +2,15 @@ package com.example.t_baymessenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_send_new_message.*
 
 class SendNewMessage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_send_new_message)
+
+        newmessage_recycler.adapter
+        newmessage_recycler.layoutManager = LinearLayoutManager(this)
     }
 }
